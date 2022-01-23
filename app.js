d83467b83825baa3c076b10a5e284fe8fd980449
Emily Morgan <emily189morgan@hotmail.com>
2022-01-22 19:15:18 -0500

    used nodemon

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 //const http = require('http');
+import express from 'express';
 
-import { createServer } from 'http';
-
+const app = express();
 const port = process.env.PORT || 3000;
 //this is the address of the web app running on you localhost
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Node Express is easy!</h1>');
-});
-//this creates a server, writes a status code and writes some content. 
+app.get('/', (req, res) => {
+    res.send('Hello from Express amazing!')
+})
+// req is "request", res is "respond".
+// the / at the begining is what ever comes after the port address. ex: localhost:3000/...
+//so above it is saying when there is nothing after the /, display the homepage. 
 
+app.get('/joe', (req,res) => {
+    res.send(`This is joes page!`)
+})
 
-server.listen(port, () => {
-  console.log(`Server running at ${port}/`);
+app.get('/john', (req,res) => {
+    res.send(`This is johns page!`)
+})
+
+app.listen(port, () => {
+  console.log(`App running at ${port}/`);
 });
 //this is the minimum that you need to write a running node app
