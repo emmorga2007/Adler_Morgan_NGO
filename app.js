235ed91b10f8212d7a789e9604c14706908f5073
Emily Morgan <emily189morgan@hotmail.com>
2022-01-27 20:12:08 -0500

    added routes and views directories

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 //const http = require('http');
 import express from 'express';
+//below imports the routes
+import router from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+
+//set up the root directory reference 
+// find the global URL using fileURLToPath
+//and then turn that into the __dirname (something like User/Desktop/NGO_Redux/)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 //this is the address of the web app running on you localhost
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express amazing!')
-})
-// req is "request", res is "respond".
-// the / at the begining is what ever comes after the port address. ex: localhost:3000/...
-//so above it is saying when there is nothing after the /, display the homepage. 
+app.use(express.static(path.join(__dirname, "public")));
+// this is saying to look in the public directory/folder for our static pages 
 
-app.get('/joe', (req,res) => {
-    res.send(`This is joes page!`)
-})
-
-app.get('/john', (req,res) => {
-    res.send(`This is johns page!`)
-})
+//tell the app to use the router file 
+//when ever it gets an incoming request it will re-route to our router file which will then parse the request and then respond back with resources
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`App running at ${port}/`);
